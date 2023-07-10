b0f3153419ffff8bd138687dd2a56e533ba402bb
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2023-07-10 21:14:13 +0600

    redux integrate

---
+++ src/layouts/Navbar.tsx
@@ -0,0 +1,102 @@
+import { Link } from 'react-router-dom';
+import { Avatar, AvatarFallback, AvatarImage } from '../components/ui/avatar';
+import { Button } from '../components/ui/button';
+import { DropdownMenuSeparator } from '../components/ui/dropdown-menu';
+import { DropdownMenuLabel } from '../components/ui/dropdown-menu';
+import {
+  DropdownMenuItem,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from '../components/ui/dropdown-menu';
+import { HiOutlineSearch } from 'react-icons/hi';
+import Cart from '../components/Cart';
+import logo from '../assets/images/technet-logo.png';
+import { useAppSelector } from '@/redux/hooks';
+
+export default function Navbar() {
+  const { user } = useAppSelector((state) => state.user);
+
+  const handleSignOut = () => {
+    console.log('sign out');
+  };
+
+  return (
+    <nav className="w-full h-16 fixed top backdrop-blur-lg z-10">
+      <div className="h-full w-full bg-white/60">
+        <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
+          <div>
+            <Link to="/">
+              <img className="h-8" src={logo} alt="log" />
+            </Link>
+          </div>
+          <div>
+            <ul className="flex items-center">
+              <li>
+                <Button variant="link" asChild>
+                  <Link to="/">Home</Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant="link" asChild>
+                  <Link to="/products">Products</Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant="link" asChild>
+                  <Link to="/checkout">Checkout</Link>
+                </Button>
+              </li>
+              <li>
+                <Button variant="ghost">
+                  <HiOutlineSearch size="25" />
+                </Button>
+              </li>
+              <li>
+                <Cart />
+              </li>
+              <li className="ml-5">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="outline-none">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="cursor-pointer">
+                      Profile
+                    </DropdownMenuItem>
+                    {!user.email ? (
+                      <>
+                        <Link to="/login">
+                          <DropdownMenuItem className="cursor-pointer">
+                            Login
+                          </DropdownMenuItem>
+                        </Link>
+                        <Link to="/signup">
+                          <DropdownMenuItem className="cursor-pointer">
+                            Sign up
+                          </DropdownMenuItem>
+                        </Link>
+                      </>
+                    ) : (
+                      <DropdownMenuItem
+                        onClick={() => handleSignOut()}
+                        className="cursor-pointer"
+                      >
+                        Logout
+                      </DropdownMenuItem>
+                    )}
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
